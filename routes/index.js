be2b133094f6abce0e95841ffd20ef981612df61
jeff-ma <jefm206@gmail.com>
2016-12-24 01:14:10 -0800

    changed code for sql server connection

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
+var sql = require("mssql");
+var result;
+
+// config for your database
+var config = {
+    user: 'INFO445',
+    password: 'GoHuskies!',
+    server: 'is-hay04.ischool.uw.edu',
+    database: 'MSJapan'
+};
+
+// connect to database
+sql.connect(config, function (err) {
+
+    if (err) console.log(err);
+
+    // create Request object
+    var request = new sql.Request();
+       
+    // query to the database and get the records
+    request.query('SELECT * FROM PRODUCT', function (err, results) {
+        
+        if (err) console.log(err);
+
+        // send records as a response
+        // res.send(recordset);
+        result = results;
+        console.log(results[0]);
+        
+    });
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'New Application' });
+  res.render('index', {title: 'New Application', result: result});
 });
 
 module.exports = router;
