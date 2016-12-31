3af6453d4038f1a257ae562e55950a32aa586c28
jeff-ma <jefm206@gmail.com>
2016-12-30 22:45:45 -0800

    added population of brands drop down list

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var sql = require("mssql");
 var results;
+var brands;
+var storage;
 
 // config for your database
 var config = {
@@ -12,7 +14,7 @@ var config = {
 };
 
 // connect to database
-sql.connect(config, function (err) {
+sql.connect(config, function(err) {
 
     if (err) {
         console.log(err);
@@ -22,25 +24,39 @@ sql.connect(config, function (err) {
     var request = new sql.Request();
        
     // query the database and get the records
-    request.query('SELECT * FROM PRODUCT', function (err, recordset) {
+    request.query('SELECT * FROM PRODUCT', function(err, recordset) {
         console.log('preparing query');
         results = recordset;
         console.log('saving results');
         console.log(recordset[1000]);
-        //console.log(recordsets.length); // count of recordsets returned by the procedure 
-        // console.log(recordsets[0].length); // count of rows contained in first recordset 
-        // console.log(returnValue); // procedure return value 
-        // console.log(recordsets.returnValue); // same as previous line
     });
+
+    // query distinct brand names 
+    request.query('SELECT DISTINCT ManufacturerName FROM MANUFACTURER JOIN PRODUCT ON MANUFACTURER.ManufacturerID=PRODUCT.ManufacturerID', function(err, recordset) {
+    	console.log('getting brands');
+    	brands = recordset;
+    	console.log('saving brands');
+    	console.log(recordset);
+    });
+
+    // request.query('SELECT DISTINCT StorageSpaceGB FROM STORAGE', function(err, recordset) {
+    // 	console.log('getting storage sizes');
+    // 	storage = recordset;
+    // 	console.log('saving storage');
+    // 	console.log(recordset);
+    // });
 });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', {title: 'Home', results: results});
+    res.render('index', {title: 'Home', results: results, brands: brands});
 });
 
 router.post('/', function(req, res, next) {
-    res.render('index', {title: 'Test', results: results});
+	console.log('getting storage');
+	var store = req.body.Storage;
+	console.log(req.body);
+    res.render('index', {title: 'Test', results: results, brands: brands});
 });
 
 module.exports = router;
